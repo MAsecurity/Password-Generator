62f7041bc500b206dd40d32ce5e48d90b6e12fe9
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-05-27 04:20:18 +0100

    Fix bug The range is inclusive so we could end up with a out of range error,which i had to decrement the length

--- password-generator.sh
+++ password-generator.sh
@@ -3,8 +3,11 @@ upper_case_letters=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 lower_case_letters=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 symbols=("$" "!" "@" "#" "{" "}" "[" "]" "*" "&")
 len_upper_array=${#upper_case_letters[@]}
-len_lower_array=${#lower_case_letters[@]}
+len_lower_array=${#lower_case_letters[@]} 
 len_symbols=${#symbols[@]}
+((len_upper_array = len_upper_array - 1))
+((len_lower_array = len_lower_array - 1))
+((len_symbols = len_symbols - 1))
 password=""
 for ((i=0; i<4; i++));
 do
